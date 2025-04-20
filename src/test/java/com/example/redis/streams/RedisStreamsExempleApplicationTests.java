45f19a75b78b9ef0428da07c76c44a0c22616c1c
Silvânio Júnior <silvanio.junior@widescope.pt>
2025-04-20 15:02:11 +0100

    Add Consumer and Producer

--- src/test/java/com/example/redis/streams/RedisStreamsExempleApplicationTests.java
+++ src/test/java/com/example/redis/streams/RedisStreamsExempleApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.redis.streams;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class RedisStreamsExempleApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		assertThat(applicationContext).isNotNull();
+	}
+
+	@Test
+	void testBeanPresence() {
+		assertThat(applicationContext.containsBean("redisTemplate")).isTrue();
 	}
 
 }
